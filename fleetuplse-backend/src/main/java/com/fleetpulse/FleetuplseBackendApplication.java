a6b17c36508b78864642ea4877a8682f8466da84
Pranjal <pranjaltiwari7188@gmail.com>
2026-07-01 10:56:15 +0530

    initial commit

---
+++ fleetuplse-backend/src/main/java/com/fleetpulse/FleetuplseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fleetpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetuplseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetuplseBackendApplication.class, args);
+	}
+
+}
